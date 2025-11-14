fa4275071f400b3140ff252863f005df9759ebbf
Shanda764 <shandadwijri7@gmail.com>
2025-11-15 02:55:52 +0700

    n

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/portofolio",
+  assetPrefix: "/portofolio",
 };
 
 export default nextConfig;
