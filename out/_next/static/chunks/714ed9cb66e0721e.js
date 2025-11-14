05eecf6364031e35b3a37bdf6af1fb31b5eeacf3
Shanda764 <shandadwijri7@gmail.com>
2025-11-15 03:43:26 +0700

    fix image path

--- out/_next/static/chunks/714ed9cb66e0721e.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/0041f99bc6a9bdf4.js",
-  "static/chunks/turbopack-bcf089cfa656412c.js"
-])
--- out/_next/static/chunks/abf48c6335d7cf3c.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/0041f99bc6a9bdf4.js",
-  "static/chunks/turbopack-c822a5eae0cdf129.js"
-])
