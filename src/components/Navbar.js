3abc8b3c7c1dc49ab097bf7419fc909e87bf4c5e
Shanda764 <shandadwijri7@gmail.com>
2025-11-15 05:34:23 +0700

    Update projects page and images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,10 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center shadow-md relative z-20">
       {/* My Portfolio bisa diklik */}
-      <Link href="/" className="text-xl font-bold hover:text-blue-400">
-        My Portfolio
-      </Link>
+     <Link href="/">
+        <a className="text-xl font-bold hover:text-blue-400">My Portfolio</a>
+     </Link>
+
 
       <div className="flex gap-6">
         <Link href="/" className="flex items-center gap-1 hover:text-blue-400">
