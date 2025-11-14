5fecae0c20cdc9a59d8770d83cfecb565c3004f5
Shanda764 <shandadwijri7@gmail.com>
2025-11-15 05:31:30 +0700

    Update projects page and images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from "react-icons/fa";
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center shadow-md relative z-20">
-      <h1 className="text-xl font-bold">My Portfolio</h1>
+      {/* My Portfolio bisa diklik */}
+      <Link href="/" className="text-xl font-bold hover:text-blue-400">
+        My Portfolio
+      </Link>
+
       <div className="flex gap-6">
         <Link href="/" className="flex items-center gap-1 hover:text-blue-400">
           <FaHome /> Home
